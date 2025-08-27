71842e90a3fa002304c71a5ab4ad92919e87c70e
Lucas Eduardo Pereira da Cruz <lucas.cruz54@fatec.sp.gov.br>
2025-08-27 12:44:31 -0300

    Meu pet sumiu, reutilizando arquivos e início de página em php

---
+++ meu_pet_sumiu/models/usuarios.class.php
@@ -0,0 +1,59 @@
+<?php
+    class usuarios 
+    {
+        public function __construct(private int $id_usuario = 0, private string $nome = "", 
+        private string $email = "", private string $senha = "", private string $celular = ""){}
+
+        public function getId_usuario($id_usuario)
+        {
+            return $this->id_usuario;
+        }
+
+        public function getNome($nome)
+        {
+            return $this->nome;
+        }
+
+        public function getEmail($email)
+        {
+            return $this->email;
+        }
+
+        public function getSenha($senha)
+        {
+            return $this->senha;
+        }
+
+        public function getCelular($celular)
+        {
+            return $this->celular;
+        }
+
+
+        //SET
+        public function setId_usuario($id_usuario)
+        {
+            return $this->id_usuario = $id_usuario;
+        }
+
+        public function setNome($nome)
+        {
+            return $this->nome = $nome;
+        }
+
+        public function setEmail($email)
+        {
+            return $this->email = $email;
+        }
+
+        public function setSenha($senha)
+        {
+            return $this->senha = $senha;
+        }
+
+        public function setCelular($celular)
+        {
+            return $this->celular = $celular;
+        }
+    } //Fim da classe
+?>
