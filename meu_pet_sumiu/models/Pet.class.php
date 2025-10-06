2a2911f38ffcb6a739f23f02bd141206307a5f7f
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2025-10-06 16:15:00 -0300

    Get e Set do pet.class.php

--- meu_pet_sumiu/models/Pet.class.php
+++ meu_pet_sumiu/models/Pet.class.php
@@ -5,7 +5,7 @@
             private string $nome = "",
             private string $idade = "",
             private string $raca = "",
-            private string $port = "",
+            private string $porte = "",
             private string $local = "",
             private string $data = "",
             private string $imagem = "",
@@ -14,7 +14,140 @@
             private string $observacoes = "",
             private string $situacao = "",
             private Usuarios $usuario = new Usuarios() ) {} 
+
+            public function getId_pet()
+        {
+            return $this->id_pet;
+        }
+
+        public function getNome()
+        {
+            return $this->nome;
+        }
+
+        public function getIdade()
+        {
+            return $this->idade;
+        }
+
+        public function getRaca()
+        {
+            return $this->raca;
+        }
+
+        public function getLocal()
+        {
+            return $this->local;
+        }
+
+        public function getData()
+        {
+            return $this->data;
+        }
+
+        public function getImagem()
+        {
+            return $this->imagem;
+        }
+
+        public function getCorOlhos()
+        {
+            return $this->cor_olhos;
+        }
+
+        public function getCor()
+        {
+            return $this->cor;
+        }
+
+        public function getObservacoes()
+        {
+            return $this->observacoes;
+        }
+
+        public function getSituacao()
+        {
+            return $this->situacao;
+        }
+
+        public function getUsuario()
+        {
+            return $this->usuario;
+        }
+
+
+        //SET
+        public function setId_pet($id_pet)
+        {
+            return $this->id_pet = $id_pet;
+        }
+
+        public function setNome($nome)
+        {
+            return $this->nome = $nome;
+        }
+
+       public function setIdade($idade)
+        {
+            return $this->idade = $idade;
+        }
+
+
+        public function setRaca($raca)
+        {
+            return $this->raca = $raca;
+        }
+
+
+        public function setLocal($local)
+        {
+            return $this->local = $local;
+        }
+
+
+        public function setData($data)
+        {
+            return $this->data = $data;
+        }
+
+
+        public function setImagem($imagem)
+        {
+            return $this->imagem = $imagem;
+        }
+
+
+        public function setCorOlhos($cor_olhos)
+        {
+            return $this->cor_olhos = $cor_olhos;
+        }
+
+
+        public function setCor($cor)
+        {
+            return $this->cor = $cor;
+        }
+
+
+        public function setObservacoes($observacoes)
+        {
+            return $this->observacoes = $observacoes;
+        }
+
+
+        public function setSituacao($situacao)
+        {
+            return $this->situacao = $situacao;
+        }
+
+
+        public function setUsuario($usuario)
+        {
+            return $this->usuario = $usuario;
+        }
+
     }
 
-    //fazer o get e o set
+    //fazer o get e o set - feito!! 
+    
 ?>
